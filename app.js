6f188b1e093719275830c16993de00fb757015ed
ROMAN <makromario@gmail.com>
2021-01-18 21:18:37 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const imagesSlides = document.querySelectorAll(".image-to-show");
+let activeSlide = document.querySelector('.active-slide');
+
+
+setInterval(slideBySlide, 3000)
+
+
+function slideBySlide() {
+
+    for (i = 0; i < imagesSlides.length; i++) {
+        if (activeSlide === imagesSlides[i]) {
+
+
+            activeSlide.classList.remove('active-slide');
+            activeSlide = imagesSlides[i + 1]
+            if (!activeSlide) {
+
+
+                activeSlide = imagesSlides[0]
+            }
+            activeSlide.classList.add('active-slide');
+            break
+
+
+        }
+    }
+
+
+}
